1ef6624f02ff26760cd8fd563e48080d42c33651
Javaro3 <99656776+Javaro3@users.noreply.github.com>
2022-09-26 15:21:48 +0300

    lab1

---
+++ Lab1/Lab1/ConvertNumber.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    internal class ConvertNumber
+    {
+        public int ConvertFrom10TO8(int num)
+        {
+            int i = 1;
+            int resault = num % 8;
+            int tmp = num / 8;
+            while (tmp >= 1)
+            {
+                resault += tmp % 8 * (int)Math.Pow(10, i);
+                tmp /= 8;
+                i++;
+            } 
+            return resault;
+        }
+    }
+}
---
+++ Lab1/Lab1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 101;
+            Console.WriteLine($"Исходное чилсо (10 система) - {num}");
+            ConvertNumber convertNumber = new ConvertNumber();
+            Console.WriteLine($"Конвертированное число (8 система) - {convertNumber.ConvertFrom10TO8(num)}");
+        }
+    }
+}
